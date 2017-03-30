d24c34bbe28c02bc25561466c81af24f0ca4fa6e
Nate Clark <nate@hired.com>
2017-03-29 21:04:07 -0700

    blink led on success; some cleanup

--- lua/application.lua
+++ lua/application.lua
@@ -10,9 +10,21 @@ globalHeaders = globalHeaders .. "Authorization: Bearer " .. auth_token .. "\r\n
 globalHeaders = globalHeaders .. "Content-Type: application/json\r\n"
 requestQueue = {}
 
+if blink_led then
+  led_pin = 4
+  gpio.mode(led_pin, gpio.OUTPUT)
+end
 --
 -- GLOBAL FUNCTIONS
 --
+
+-- Blink the onboard LED
+function blinkLed()
+  gpio.write(led_pin, gpio.LOW)
+  tmr.create():alarm(100, tmr.ALARM_SINGLE, function()
+    gpio.write(led_pin, gpio.HIGH)
+  end)
+end
 
 -- Inserts a request to the end of the queue
 function queueRequest(sensorId, value)
@@ -20,13 +32,19 @@ function queueRequest(sensorId, value)
   table.insert(requestQueue, requestData)
 end
 
+function jsonPayload(sensorData)
+  return [[{"sensor_id":"]] .. sensorData.sensorId .. [[","state":]] .. sensorData.value .. "}"
+end
+
 -- Constructs a POST request to SmartThings to change the state of a sensor
 function doNextRequest()
   local sensorData = requestQueue[1]
   if sensorData then
-      local payload = [[{"sensor_id":"]] .. sensorData.sensorId .. [[","state":]] .. sensorData.value .. "}"
+      local payload = jsonPayload(sensorData)
+      -- set http headers
       local headers = globalHeaders .. "Content-Length: " .. string.len(payload) .. "\r\n"
 
+      -- do the POST to SmartThings
       http.post(
         apiHost .. apiEndpoint,
         headers,
@@ -35,12 +53,14 @@ function doNextRequest()
             if code == 201 then
               print("Success: " .. sensorData.sensorId .. " = " .. sensorData.value)
               table.remove(requestQueue, 1) -- remove from the queue when successful
+              if blink_led then blinkLed() end
             elseif code > 201 then
               print("Error " .. code .. " posting " .. sensorData.sensorId .. ", retrying")
             end
           end)
   end
 end
+
 
 --
 -- MAIN LOGIC
@@ -50,10 +70,8 @@ end
 for i,sensor in pairs(sensors) do
   gpio.mode(sensor.gpioPin, gpio.INPUT, gpio.PULLUP)
   sensor.state = gpio.read(sensor.gpioPin)
-  if report_on_startup then
-    queueRequest(sensor.deviceId, sensor.state)
-  end
-  
+  queueRequest(sensor.deviceId, sensor.state, true)
+
   gpio.trig(sensor.gpioPin, "both", function (level)
     local newState = gpio.read(sensor.gpioPin)
     if sensor.state ~= newState then
